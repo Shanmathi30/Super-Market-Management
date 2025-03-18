d14a9323c54d908ab27821edba39c787260238f9
Shanmathi <shanmathianbu2@gmail.com>
2025-03-18 15:55:39 +0530

    css style product dashboard

--- Public/Assests/Js files/navigation.js
+++ Public/Assests/Js files/navigation.js
@@ -1,28 +1,12 @@
 class SuperMarketNavigation extends HTMLElement {
-
     constructor () {
         super ();
-
         const clonedNode = document.getElementById("sm_supermarket_navigation_template").content.cloneNode(true);
         this.appendChild(clonedNode); 
     }
-
     connectedCallback () {
         const urlParams = new URLSearchParams(window.location.search);
         urlParams.get("isAdmin") === "N" ? $(".customer").removeClass("d-none") : $(".admin").removeClass("d-none");
     }
 }
 customElements.define("supermarket-navigation", SuperMarketNavigation);
-
-/*class SuperMarketNavigation extends HTMLElement{
-
-    constructor(){
-        super();
-
-        const template = document.getElementById("sm_supermarket_navigation_template");
-        console.log(template)
-        this.appendChild(template.content.cloneNode(true));
-    }
-}
-
-customElements.define("supermarket-navigation", SuperMarketNavigation);*/
